d1557893d4c1565ade081fe520e9a83d69c7de00
oscarroyo4 <47557315+oscarroyo4@users.noreply.github.com>
2020-12-18 20:04:30 +0100

    working firebase

--- firebase/lib/main.dart
+++ firebase/lib/main.dart
@@ -1,6 +1,11 @@
-import 'package:flutter/material.dart';
+import 'dart:typed_data';
+import 'dart:ui';
+
 import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+
+import 'model.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,46 +15,149 @@ Future<void> main() async {
 
 class FirebaseApp extends StatelessWidget {
   // This widget is the root of your application.
+
+  Widget _buildError(error) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          error.toString(),
+          style: TextStyle(color: Colors.red),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLoading() {
+    return Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+
+  Widget _makeImagesGrid(List<Item> docs) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Shop'),
+      ),
+      body: GridView.builder(
+          itemCount: docs.length,
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          itemBuilder: (context, index) {
+            final item = docs[index];
+            return ImageGridItem(item);
+          }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: HomePage(),
+      home: StreamBuilder(
+        stream: todoListSnapshots(),
+        builder: (context, AsyncSnapshot<List<Item>> snapshot) {
+          if (snapshot.hasError) {
+            return _buildError(snapshot.error);
+          }
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return _buildLoading();
+            case ConnectionState.active:
+              return _makeImagesGrid(snapshot.data);
+            default: // ConnectionState.none || ConnectionState.done
+              return _buildError("Unreachable (done or none)");
+          }
+        },
+      ),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class ImageGridItem extends StatefulWidget {
+  String _name;
+  String _imageName;
+  String _price;
+
+  ImageGridItem(Item doc) {
+    this._name = doc.name;
+    this._imageName = doc.image;
+    this._price = doc.price.toString();
+  }
+
+  @override
+  _ImageGridItemState createState() => _ImageGridItemState();
+}
+
+class _ImageGridItemState extends State<ImageGridItem> {
+  Uint8List imageFile;
+  Reference photosReference = FirebaseStorage.instance.ref().child('photos');
+  Color buttonColor = Colors.white;
+
+  getImage() {
+    int MAX_SIZE = 5 * 1024 * 1024;
+    photosReference.child(widget._imageName).getData(MAX_SIZE).then((data) {
+      this.setState(() {
+        imageFile = data;
+      });
+    }).catchError((error) {});
+  }
+
+  Widget decideGridTileWidget() {
+    if (imageFile == null) {
+      return Center(child: Text('no data'));
+    } else {
+      return Image.memory(
+        imageFile,
+        fit: BoxFit.fitHeight,
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getImage();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final db = FirebaseFirestore.instance;
-    return Scaffold(
-      appBar: AppBar(title: Text('DM Frontend')),
-      body: StreamBuilder(
-        stream: db
-            .collection('users')
-            .where('age', isGreaterThan: 20)
-            .orderBy('age', descending: true)
-            .snapshots(),
-        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (!snapshot.hasData) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          final query = snapshot.data;
-          final documents = query.docs;
-          return ListView.builder(
-            itemCount: documents.length,
-            itemBuilder: (context, index) {
-              final user = documents[index];
-              return ListTile(
-                title: Text('${user['first_name']} ${user['last_name']}'),
-                subtitle: Text('${user['age']}'),
-              );
-            },
-          );
-        },
+    return GridTile(
+      child: MaterialButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 8,
+              child: decideGridTileWidget(),
+            ),
+            Expanded(
+              flex: 2,
+              child: Row(children: [
+                Expanded(
+                  flex: 5,
+                  child: Text(widget._name),
+                ),
+                Expanded(
+                    flex: 5,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            topRight: Radius.circular(10)),
+                        color: Colors.amber,
+                      ),
+                      child: Center(child: Text('${widget._price}€')),
+                    )),
+              ]),
+            ),
+          ],
+        ),
+        color: buttonColor,
+        onPressed: () => print('hello'),
       ),
     );
   }
--- firebase/lib/model.dart
+++ firebase/lib/model.dart
@@ -1,29 +1,31 @@
+import 'dart:ffi';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Item {
   String id;
-  String what;
-  bool done;
-  DateTime date;
+  String name;
+  num price;
+  String image;
 
   Item.fromFirestore(DocumentSnapshot doc) {
     this.id = doc.id;
-    this.what = doc['what'];
-    this.done = doc['done'];
-    this.date = (doc['date'] as Timestamp).toDate();
+    this.name = doc['name'];
+    this.price = doc['price'];
+    this.image = doc['image'];
   }
 
   Map<String, dynamic> toFirestore() => {
         'id': id,
-        'what': what,
-        'done': done,
-        'date': date,
+        'name': name,
+        'price': price,
+        'image': image,
       };
 }
 
 Stream<List<Item>> todoListSnapshots() {
-  final todos = FirebaseFirestore.instance.collection('todos');
-  return todos.orderBy('date').snapshots().map((QuerySnapshot query) {
+  final products = FirebaseFirestore.instance.collection('products');
+  return products.snapshots().map((QuerySnapshot query) {
     List<Item> result = [];
     for (var doc in query.docs) {
       result.add(Item.fromFirestore(doc));
